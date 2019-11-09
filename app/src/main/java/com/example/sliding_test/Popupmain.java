d7ca07c87147824b10616e78c9f623166ae0f1a0
jgw1031 <jgw1031@naver.com>
2019-11-09 20:39:09 +0900

    수동 리스트 과정
    수동 리스트 과정

--- app/src/main/java/com/example/sliding_test/Popupmain.java
+++ app/src/main/java/com/example/sliding_test/Popupmain.java
@@ -16,7 +16,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Popupmain extends Activity {
     ImageButton button1;
-    public String Selects="1호선";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //타이틀바 없애기
@@ -26,21 +25,26 @@ public class Popupmain extends Activity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Selects="2호선";
+                Intent intent = new Intent (Popupmain.this,Popupmain.class);
+                intent.putExtra("name","1호선");
+                startActivityForResult(intent, 1);
+                finish();
             }
         });
-
+        Intent intent = getIntent(); /*데이터 수신*/
+        String list = intent.getExtras().getString("name");
         Api_adrss api_adrss = new Api_adrss();
-        String Search =api_adrss.trains(Selects);
-        String[] Searchs =Search.split("/");
-        String Current =Searchs[0];
-        String Trains =Searchs[1];
-        String Arrival =Searchs[2];
-        String[] list1 =Current.split(",");
-        String[] list2 =Arrival.split(",");
-        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.popup_items,R.id.itemss1,list1) ;
-        ArrayAdapter adapter2 = new ArrayAdapter(this, R.layout.popup_items,R.id.itemss2,list2) ;
-        ListView listview = (ListView) findViewById(R.id.popuplist) ;
-        listview.setAdapter(adapter) ;
+        String Search = api_adrss.trains(list);
+        System.out.println(Search);
+        String[] Searchs = Search.split("/");
+        String Current = Searchs[0];
+        String Trains = Searchs[1];
+        String Arrival = Searchs[2];
+        String[] list1 = Current.split(",");
+        String[] list2 = Arrival.split(",");
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.popup_items, R.id.itemss1, list1);
+        ListView listview = (ListView) findViewById(R.id.popuplist);
+        listview.setAdapter(adapter);
+
     }
 }
