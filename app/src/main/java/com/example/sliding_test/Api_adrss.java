ba04981222088399259534f73c5e0d0563e8efd0
jgw1031 <jgw1031@naver.com>
2019-11-09 19:29:31 +0900

    팝업 중간과정
    팝업 중간과정

--- app/src/main/java/com/example/sliding_test/Api_adrss.java
+++ app/src/main/java/com/example/sliding_test/Api_adrss.java
@@ -7,7 +7,8 @@ import java.net.URL;
 
 
 public class Api_adrss  {
-    String adrs,index_y,index_x,index_stayion,index_line,index_btrainNo;
+    String adrs,index_y,index_x,index_stayion,index_line,index_btrainNo,line;
+    String index_trains="열차",index_Arrival="도착역",index_Current="현재역";
     public String adrss(String string2,String string1){
         BufferedReader br = null;
         BufferedReader br1 = null;
@@ -81,4 +82,36 @@ public class Api_adrss  {
         }
         return index_btrainNo;
     }
+    public String trains(String lines){
+        try{
+            for(int i =1 ; i<50;i++) {
+                BufferedReader br4 = null;
+                String urlstr = "http://swopenAPI.seoul.go.kr/api/subway/6a4565616c6a6777393145686e4972/json/realtimePosition/"+i+"/"+i+"/"+lines;
+                URL url = new URL(urlstr);
+                HttpURLConnection urlconnection = (HttpURLConnection) url.openConnection();
+                urlconnection.setRequestMethod("GET");
+                br4 = new BufferedReader(new InputStreamReader(urlconnection.getInputStream(), "UTF-8"));
+                String result4 = "";
+                String line4;
+                if ((line4 = br4.readLine()) != null)
+                    result4 = result4 + line4 + "\n";
+                int target_statnm = result4.indexOf("\"statnNm\":\"") + 11;
+                int target_trainNo = result4.indexOf("\",\"trainNo\":\"")+13;
+                int target_statntnm= result4.indexOf("statnTnm\":\"")+11;
+                index_Current= index_Current +","+result4.substring(target_statnm, (result4.substring(target_statnm).indexOf("\",\"trainNo\":\"")  + target_statnm));     //현재역
+                index_trains= index_trains +","+result4.substring(target_trainNo, (result4.substring(target_trainNo).indexOf("\",\"lastRecptnDt") + target_trainNo));     //열차아이디
+                index_Arrival=index_Arrival +","+result4.substring(target_statntnm, (result4.substring(target_statntnm).indexOf("\",\"trainSttus") + target_statntnm));     //도착역
+            }
+/*
+            String[] Current =index_Current.split(",");
+            String[] Trains =index_trains.split(",");
+            String[] Arrival =index_Arrival.split(",");
+            for (int i=0 ;i<=50;i++)
+            System.out.println(Current[i]+Trains[i]+Arrival[i]);*/
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+        return index_Current+"/"+index_trains+"/"+index_Arrival+";";
+    }
 }
+
--- app/src/main/java/com/example/sliding_test/Popupmain.java
+++ app/src/main/java/com/example/sliding_test/Popupmain.java
@@ -1,18 +1,46 @@
 package com.example.sliding_test;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Popupmain extends Activity {
+    ImageButton button1;
+    public String Selects="1호선";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //타이틀바 없애기
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.popup_activity);
+        button1=(ImageButton)findViewById(R.id.stayline1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Selects="2호선";
+            }
+        });
+
+        Api_adrss api_adrss = new Api_adrss();
+        String Search =api_adrss.trains(Selects);
+        String[] Searchs =Search.split("/");
+        String Current =Searchs[0];
+        String Trains =Searchs[1];
+        String Arrival =Searchs[2];
+        String[] list1 =Current.split(",");
+        String[] list2 =Arrival.split(",");
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.popup_items,R.id.itemss1,list1) ;
+        ArrayAdapter adapter2 = new ArrayAdapter(this, R.layout.popup_items,R.id.itemss2,list2) ;
+        ListView listview = (ListView) findViewById(R.id.popuplist) ;
+        listview.setAdapter(adapter) ;
     }
 }
