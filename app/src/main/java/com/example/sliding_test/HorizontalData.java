1f1d38e8eb3beff70bf10800c3c3bfd9c7ee8222
ojs7408 <dhwna1324@naver.com>
2019-11-15 23:34:34 +0900

    Code update
    Code update

--- app/src/main/java/com/example/sliding_test/HorizontalData.java
+++ app/src/main/java/com/example/sliding_test/HorizontalData.java
@@ -1,22 +1,21 @@
 package com.example.sliding_test;
 
-public class VerticalData {
+public class HorizontalData { //포화도, 노약자석에 대한 이미지 + 수치값 객체 자료구조 데이터형을 위한 클래스
 
     private int img;
     private String text;
 
-    public VerticalData(int img, String text){
+    public HorizontalData(int img, String text){
         this.img = img;
         this.text = text;
     }
 
     public String getText(){
         return this.text;
-    }
+    } // text 반환
 
     public int getImg(){
         return this.img;
-    }
-
+    } // 이미지 반환
 
 }
